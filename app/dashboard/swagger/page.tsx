5a1a422d7e68703677238ea8d07b43d5f0362937
douglas <douglasmaxton@gmail.com>
2025-02-04 15:36:30 +0000

    feat(changed): swagger page displays in dev mode

--- app/dashboard/swagger/page.tsx
+++ app/dashboard/swagger/page.tsx
@@ -5,8 +5,14 @@ import "swagger-ui-react/swagger-ui.css";
 
 export default function Page() {
   return (
-    <div style={{ height: "100vh" }}>
-      <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
-    </div>
+    <>
+      {process.env.NODE_ENV === "development" ? (
+        <div style={{ height: "100vh" }}>
+          <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+        </div>
+      ) : (
+        <p>This Page is only for Development</p>
+      )}
+    </>
   );
 }
